9a31877fbb8ef4b530295bb2d29d86c2989b3ce3
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-17 09:55:53 -0700

    xpress created, home page accessible
    commit comment didnt show the first time

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080; // default port is 8080
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
