435594e9b600a851d4e9916a700e6018273dc123
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-20 17:42:14 -0700

    databases file initial commit
    separating the databases from the main code in order to keep the main code dry.

---
+++ databases.js
@@ -0,0 +1,7 @@
+//database constants to be called into express_server.js
+//removed key value pairs in urldatabase so that the user can populate it with their own short urls
+const urlDatabase = {};
+
+const users = {};
+
+module.exports = { urlDatabase, users };
